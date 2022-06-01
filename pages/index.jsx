002d26614c41cd5ec5fe84be04817a16dfd45d5b
Vijay Kesanakurthi <70144210+VijayKesanakurthi@users.noreply.github.com>
2022-06-01 06:23:03 +0000

    ui improvements

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,9 +5,14 @@ import Header from '../components/header'
 
 export default function Home() {
   return (
+    <>
+    <Head>
+    <title>Block Chain Demo</title>  
+    </Head>
    <div className="bg-gray-50 min-h-screen flex flex-col py-2 space-y-5 justify-center items-center">
     <Header/>
     <BlockChain/>
    </div>
+      </>
   )
 }
